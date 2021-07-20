199d9f076786ca1fe6083ef7a855d7475a225c10
Rasa Jurgutytė <rjurgutyte@alna.lt>
2021-07-20 21:35:39 +0300

    first remote commit

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,20 +1,68 @@
+import React, {useState} from 'react';
 import './styles/ExpenseForm.css';
 
+interface newExpense{
+    title: string,
+    amount: string,
+    date: string
+}
+
 const ExpenseForm: React.FC = () => {
+
+    const [expense, setExpense] = useState<newExpense>({
+        title: "",
+        amount: "",
+        date: ""
+    });
+
+    const titleChangeHandler = (event :React.ChangeEvent<HTMLInputElement>) => {
+        setExpense((prevState) => {
+            return {...prevState,
+            title: event.target.value}
+        });
+    }
+
+    const amountChangeHandler = (event :React.ChangeEvent<HTMLInputElement>) => {
+        setExpense((prevState) => {
+            return {...prevState,
+            amount: event.target.value}
+        });
+    }
+
+    const dateChangeHandler = (event :React.ChangeEvent<HTMLInputElement>) => {
+        setExpense((prevState) => {
+            return {...prevState,
+            date: event.target.value}
+        });
+    }
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("submitted");
+
+        let submittedExpense = {
+            title: expense.title,
+            amount: parseFloat(expense.amount),
+            date: new Date(expense.date).toJSON()
+        };
+
+        console.log(submittedExpense);
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input type="text" name="" id="" />
+                    <label>Title {expense.title}</label>
+                    <input type="text" onChange={titleChangeHandler} name='expenseTitle' value={expense.title}/>
                 </div>
                 <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01'/>
+                    <label>Amount {expense.amount}</label>
+                    <input type="number" onChange={amountChangeHandler} min='0.01' step='0.01'/>
                 </div>
                 <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type="date" max ={new Date().toJSON().slice(0,10)} />
+                    <label>Date {expense.date}</label>
+                    <input type="date" onChange={dateChangeHandler} max ={new Date().toJSON().slice(0,10)} />
                 </div>
             </div>
             <div className='new-expense__actions'>
